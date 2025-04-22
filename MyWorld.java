b27ae1d971ed53364f5f24bfcd3850fa913e9851
senaG1 <161520172+senaG1@users.noreply.github.com>
2025-04-22 14:09:24 -0400

    Added rocket to my world

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Rocket rocket = new Rocket();
+        addObject(rocket, 600, 100);
     }
 }
