ca29489172ea3dd5a4fde5efd86eb6596f7acfe9
CC-iam <1202manman@gmail.com>
2025-04-23 13:59:31 -0400

    Add Seal

--- MyWorld.java
+++ MyWorld.java
@@ -4,6 +4,8 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
         
+        Seal seal = new Seal(); 
+        addObject(seal, 100, 100);
         Rocket rocket = new Rocket();
         addObject(rocket, 600, 100);
     }
